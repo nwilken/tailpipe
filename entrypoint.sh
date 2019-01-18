33209303d9af74cd7994430e2f8d9a6b79e44c67
Nate Wilken <nwilken@users.noreply.github.com>
2019-01-18 00:12:14 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,5 @@
 set -e
 
 [ -n "$STDOUT_PIPE" ] && ! [ -p "$STDOUT_PIPE" ] && mkfifo -m 666 "$STDOUT_PIPE"
-[ -n "$STDERR_PIPE" ] && ! [ -p "$STDERR_PIPE" ] && mkfifo -m 666 "$STDERR_PIPE"
 
 exec "$@"
